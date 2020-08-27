1b86b9fd65a99920000c98dae27a1954c94cdcbb
Sujit Aluru <69410505+xfre32@users.noreply.github.com>
2020-08-27 13:07:42 +0000

    Files uploaded

---
+++ index1.js
@@ -0,0 +1,78 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonHTML = this.innerHTML;
+    makeSound(buttonHTML);
+    buttonAnimation(buttonHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+function makeSound(value) {
+
+  switch (value) {
+    case "w":
+      var tom1 = new Audio("sounds/s1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/s2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/s3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/s4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/s5.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/s6.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kickBass = new Audio("sounds/s7.mp3");
+      kickBass.play();
+      break;
+
+    default: console.log(value);
+
+  }
+}
+
+function buttonAnimation(value) {
+  var activeButton = document.querySelector("." +value);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  },100);
+}
+
+
+
+
+
+
+
+
+
+//
+// var audio = new Audio("sounds/s1.mp3");
+// audio.play();
